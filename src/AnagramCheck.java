b14bb93502f66e5e0329c88de7fc216d320b630d
jestiii <jestinethomas556@gmail.com>
2026-02-06 18:51:39 +0530

    added

--- src/AnagramCheck.java
+++ src/AnagramCheck.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class AnagramCheck {
-   
-        // TODO: Read the two strings
-        // TODO: Convert both to lowercase (to handle case-insensitivity)
-        // TODO: Check if lengths are equal. If not, print "false" and return.
-        // TODO: Convert strings to char arrays
-        // TODO: Sort both char arrays
-        // TODO: Compare the sorted arrays. Print "true" if equal, else "false".
-  
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            System.out.println("false");
+            return;
+        }
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        sc.close();
+    }
 }
